3b528911537ac8f4616c0f1a6076b147ae15dd02
Paras Sharma <mail2paras.s@gmail.com>
2022-01-15 21:39:50 +0530

    replace cancel with snooze

--- android/app/src/main/kotlin/app/tummo/test_flickering/NotificationBlocker.kt
+++ android/app/src/main/kotlin/app/tummo/test_flickering/NotificationBlocker.kt
@@ -1,6 +1,5 @@
 package app.tummo.test_flickering
 
-
 import android.content.*
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
@@ -8,15 +7,15 @@ import android.util.Log
 
 class NotificationBlocker : NotificationListenerService() {
 
-
     override fun onNotificationPosted(notification: StatusBarNotification) {
         Log.d("NotificationBlocker", notification.packageName)
         Log.d("NotificationBlocker", notification.notification.channelId)
 
-        if (notification.packageName.equals(packageName) && notification.notification.channelId.equals("1")) {
+        if (notification.packageName.equals(packageName) &&
+                        notification.notification.channelId.equals("1")
+        ) {
             Log.d("NotificationBlocker", "Cancelling Notification")
-            cancelNotification(notification.key)
+            snoozeNotification(notification.key, 1000 * 60)
         }
     }
-
 }
